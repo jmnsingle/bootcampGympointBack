741ee2a72053b51e293888d731e5199873f34ac4
jmnsingle <jmnogueira2011@gmail.com>
2020-01-03 21:06:08 -0300

    Retirando config de envio de email do arquivo .env.

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
+  host: 'smtp.mailtrap.io',
+  port: 2525,
   secure: false,
   auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
+    user: 'db476cb8e6e298',
+    pass: '8f7e9498788d72',
   },
   default: {
     from: 'Equipe Gympoint <noreply@gympoint>',
